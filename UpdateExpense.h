05794b9f46644067ca87be0a4c1300d2d16c4e62
Andrew Acosta <andrew.acosta02@utrgv.edu>
2021-11-15 00:14:10 -0600

    Made it so the date updates itself onto the input box for UpdateExpense.h. Also made it load the recent expenses on its own. Also hid the mainhub since it is no longer necessary to keep it showing now that the user can view their expenses from the UpdateExpense form.

--- UpdateExpense.h
+++ UpdateExpense.h
@@ -15,8 +15,12 @@ namespace GUIpractice {
 	/// </summary>
 	public ref class UpdateExpense : public System::Windows::Forms::Form
 	{
-	public:
+	private:
 		String^ username;
+		DateTime localDate = DateTime::Now;
+		String^ dateString = localDate.ToString("MM/dd/yyyy");
+		// Create table to be used
+		DataTable^ table = gcnew DataTable();
 	public:
 		UpdateExpense(void)
 		{
@@ -30,12 +34,15 @@ namespace GUIpractice {
 			InitializeComponent();
 			username = user;
 			dataGridView1->DataSource = 0;
+			// Connects to server
 			SqlConnection^ con = gcnew  SqlConnection("Data Source=72.180.160.215,1433;Initial Catalog=expTrackerApp;Persist Security Info=True;User ID=3340project;Password=expensetracker");
 			con->Open();
-			SqlDataAdapter^ adapter = gcnew SqlDataAdapter("SELECT * FROM expense ", con);
-			DataTable^ table = gcnew DataTable();
+			// Gets desired data for table
+			SqlDataAdapter^ adapter = gcnew SqlDataAdapter("SELECT expense_name, expense_amount, expense_attribute, expense_date FROM [expTrackerApp].[dbo].[expense2] WHERE user_name='" + username + "'", con);
+			// Fills table with desired data
 			adapter->Fill(table);
 			dataGridView1->DataSource = table;
+			// Closes connection
 			con->Close();
 			//
 			//TODO: Add the constructor code here
@@ -102,18 +109,20 @@ namespace GUIpractice {
 			this->label1->AutoSize = true;
 			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(12, 36);
+			this->label1->Location = System::Drawing::Point(16, 44);
+			this->label1->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(121, 20);
+			this->label1->Size = System::Drawing::Size(152, 25);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Expense Name:";
 			this->label1->Click += gcnew System::EventHandler(this, &UpdateExpense::Label1_Click);
 			// 
 			// textBox_Name
 			// 
-			this->textBox_Name->Location = System::Drawing::Point(16, 60);
+			this->textBox_Name->Location = System::Drawing::Point(21, 74);
+			this->textBox_Name->Margin = System::Windows::Forms::Padding(4);
 			this->textBox_Name->Name = L"textBox_Name";
-			this->textBox_Name->Size = System::Drawing::Size(196, 20);
+			this->textBox_Name->Size = System::Drawing::Size(260, 22);
 			this->textBox_Name->TabIndex = 1;
 			this->textBox_Name->TextChanged += gcnew System::EventHandler(this, &UpdateExpense::TextBox_Name_TextChanged);
 			// 
@@ -122,17 +131,19 @@ namespace GUIpractice {
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label2->Location = System::Drawing::Point(12, 88);
+			this->label2->Location = System::Drawing::Point(16, 108);
+			this->label2->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(135, 20);
+			this->label2->Size = System::Drawing::Size(168, 25);
 			this->label2->TabIndex = 2;
 			this->label2->Text = L"Expense Amount:";
 			// 
 			// textBox_Amount
 			// 
-			this->textBox_Amount->Location = System::Drawing::Point(16, 111);
+			this->textBox_Amount->Location = System::Drawing::Point(21, 137);
+			this->textBox_Amount->Margin = System::Windows::Forms::Padding(4);
 			this->textBox_Amount->Name = L"textBox_Amount";
-			this->textBox_Amount->Size = System::Drawing::Size(196, 20);
+			this->textBox_Amount->Size = System::Drawing::Size(260, 22);
 			this->textBox_Amount->TabIndex = 3;
 			// 
 			// label3
@@ -140,9 +151,10 @@ namespace GUIpractice {
 			this->label3->AutoSize = true;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label3->Location = System::Drawing::Point(12, 134);
+			this->label3->Location = System::Drawing::Point(16, 165);
+			this->label3->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(77, 20);
+			this->label3->Size = System::Drawing::Size(98, 25);
 			this->label3->TabIndex = 4;
 			this->label3->Text = L"Category:";
 			// 
@@ -151,27 +163,31 @@ namespace GUIpractice {
 			this->label4->AutoSize = true;
 			this->label4->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label4->Location = System::Drawing::Point(12, 185);
+			this->label4->Location = System::Drawing::Point(16, 228);
+			this->label4->Margin = System::Windows::Forms::Padding(4, 0, 4, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(114, 20);
+			this->label4->Size = System::Drawing::Size(138, 25);
 			this->label4->TabIndex = 6;
 			this->label4->Text = L"Date Updated:";
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(16, 208);
+			this->textBox2->Location = System::Drawing::Point(21, 256);
+			this->textBox2->Margin = System::Windows::Forms::Padding(4);
 			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(196, 20);
+			this->textBox2->Size = System::Drawing::Size(260, 22);
 			this->textBox2->TabIndex = 7;
-			this->textBox2->Text = L"DD/MM/YYYY";
+			this->textBox2->Text = dateString;
+			this->textBox2->TextChanged += gcnew System::EventHandler(this, &UpdateExpense::textBox2_TextChanged);
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button1->Location = System::Drawing::Point(16, 243);
+			this->button1->Location = System::Drawing::Point(21, 299);
+			this->button1->Margin = System::Windows::Forms::Padding(4);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(110, 39);
+			this->button1->Size = System::Drawing::Size(147, 48);
 			this->button1->TabIndex = 8;
 			this->button1->Text = L"Update";
 			this->button1->UseVisualStyleBackColor = true;
@@ -181,11 +197,12 @@ namespace GUIpractice {
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->button2->Location = System::Drawing::Point(132, 243);
+			this->button2->Location = System::Drawing::Point(176, 299);
+			this->button2->Margin = System::Windows::Forms::Padding(4);
 			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(109, 39);
+			this->button2->Size = System::Drawing::Size(145, 48);
 			this->button2->TabIndex = 9;
-			this->button2->Text = L"Cancle";
+			this->button2->Text = L"Cancel";
 			this->button2->UseVisualStyleBackColor = true;
 			this->button2->Click += gcnew System::EventHandler(this, &UpdateExpense::Button2_Click);
 			// 
@@ -193,35 +210,39 @@ namespace GUIpractice {
 			// 
 			this->comboBox1->FormattingEnabled = true;
 			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(4) { L"General", L"Transportation", L"Personal", L"Buisness" });
-			this->comboBox1->Location = System::Drawing::Point(16, 157);
+			this->comboBox1->Location = System::Drawing::Point(21, 193);
+			this->comboBox1->Margin = System::Windows::Forms::Padding(4);
 			this->comboBox1->Name = L"comboBox1";
-			this->comboBox1->Size = System::Drawing::Size(196, 21);
+			this->comboBox1->Size = System::Drawing::Size(260, 24);
 			this->comboBox1->TabIndex = 10;
 			this->comboBox1->Text = L"None";
 			// 
 			// dataGridView1
 			// 
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridView1->Location = System::Drawing::Point(299, 36);
+			this->dataGridView1->Location = System::Drawing::Point(399, 44);
+			this->dataGridView1->Margin = System::Windows::Forms::Padding(4);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(306, 246);
+			this->dataGridView1->RowHeadersWidth = 51;
+			this->dataGridView1->Size = System::Drawing::Size(574, 303);
 			this->dataGridView1->TabIndex = 11;
 			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &UpdateExpense::DataGridView1_CellContentClick);
 			// 
 			// button3
 			// 
-			this->button3->Location = System::Drawing::Point(496, 288);
+			this->button3->Location = System::Drawing::Point(609, 355);
+			this->button3->Margin = System::Windows::Forms::Padding(4);
 			this->button3->Name = L"button3";
-			this->button3->Size = System::Drawing::Size(109, 37);
+			this->button3->Size = System::Drawing::Size(145, 46);
 			this->button3->TabIndex = 12;
 			this->button3->Text = L"Refresh";
 			this->button3->UseVisualStyleBackColor = true;
 			// 
 			// UpdateExpense
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(626, 379);
+			this->ClientSize = System::Drawing::Size(986, 466);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->comboBox1);
@@ -234,6 +255,7 @@ namespace GUIpractice {
 			this->Controls->Add(this->label2);
 			this->Controls->Add(this->textBox_Name);
 			this->Controls->Add(this->label1);
+			this->Margin = System::Windows::Forms::Padding(4);
 			this->Name = L"UpdateExpense";
 			this->Text = L"UpdateExpense";
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
@@ -288,6 +310,8 @@ private: System::Void DataGridView1_CellContentClick(System::Object^ sender, Sys
 
 	
 
+}
+private: System::Void textBox2_TextChanged(System::Object^ sender, System::EventArgs^ e) {
 }
 };
 }
